16bface83c87b421f83b14a5b72bc62acaf3f358
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-06-23 15:20:54 +0600

    PH-Recon.sh

--- PH-Recon.sh
+++ PH-Recon.sh
@@ -7,15 +7,12 @@ resolver="/root/wordlist/resolvers.txt"
 domain_enum(){
 for domain in $(cat $host);
 do
-mkdir -p /root/PH-Recon/$domain/subdomain /root/PH-Recon/$domain/subdomain/good /root/PH-Recon/$domain/subdomain/good/Gen_sub /root/PH-Recon/$domain/Subomain-Takeover /root/PH-Recon/$domain/Subomain-Screenshots /root/PH-Recon/$domain/Special_subdomain /root/PH-Recon/$domain/Special_subdomain/scan /root/PH-Recon/$domain/scan  /root/PH-Recon/$domain/scan/my-jaeles /root/PH-Recon/$domain/scan/jaeles /root/PH-Recon/$domain/scan/jaeles/my-url /root/PH-Recon/$domain/scan/jaeles/url /root/PH-Recon/$domain/dri  /root/PH-Recon/$domain/scan/nuclei/Php-My-Admin /root/PH-Recon/$domain/scan/nuclei /root/PH-Recon/$domain/scan/new-nuclei /root/PH-Recon/$domain/url /root/PH-Recon/$domain/Secret-api /root/PH-Recon/$domain/gf /root/PH-Recon/$domain/xss /root/PH-Recon/$domain/sql /root/PH-Recon/$domain/js_url /root/PH-Recon/$domain/git_dork /root/PH-Recon/$domain/SQL
+mkdir -p /root/PH-Recon/$domain/subdomain /root/PH-Recon/$domain/subdomain/good  /root/PH-Recon/$domain/Subomain-Takeover /root/PH-Recon/$domain/Subomain-Screenshots /root/PH-Recon/$domain/Special_subdomain /root/PH-Recon/$domain/Special_subdomain/scan /root/PH-Recon/$domain/scan  /root/PH-Recon/$domain/scan/my-jaeles /root/PH-Recon/$domain/scan/jaeles /root/PH-Recon/$domain/scan/jaeles/my-url /root/PH-Recon/$domain/scan/jaeles/url /root/PH-Recon/$domain/dri  /root/PH-Recon/$domain/scan/nuclei/Php-My-Admin /root/PH-Recon/$domain/scan/nuclei /root/PH-Recon/$domain/scan/new-nuclei /root/PH-Recon/$domain/url /root/PH-Recon/$domain/Secret-api /root/PH-Recon/$domain/gf /root/PH-Recon/$domain/xss /root/PH-Recon/$domain/sql /root/PH-Recon/$domain/js_url /root/PH-Recon/$domain/git_dork /root/PH-Recon/$domain/SQL
 
 subfinder -d $domain -all -o /root/PH-Recon/$domain/subdomain/subfinder.txt
 assetfinder -subs-only $domain | tee /root/PH-Recon/$domain/subdomain/assetfinder.txt 
 findomain -t $domain | tee /root/PH-Recon/$domain/subdomain/findomain.txt
-#sudomy -d $domain -o /root/PH-Recon/$domain/subdomain/sudomy.txt
-amass enum -active -d $domain -o /root/PH-Recon/$domain/subdomain/amass_sub.txt
 amass enum -passive -d $domain -o /root/PH-Recon/$domain/subdomain/amass_sub_passive.txt
-chaos -d $domain -o /root/PH-Recon/$domain/subdomain/chaos_sub.txt
 curl --insecure --silent "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | sed "/@/d" | sed -e 's/\.$//' | sort -u | tee /root/PH-Recon/$domain/subdomain/web.archive.txt
 curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee /root/PH-Recon/$domain/subdomain/crtsub.txt
 curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee /root/PH-Recon/$domain/subdomain/riddlersub.txt
